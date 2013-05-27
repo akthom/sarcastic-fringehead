52253cf0c5a83f6941896728b302dee1850fdd8d
Andrea Thomer <andrea.thomer@gmail.com>
2013-05-27 13:14:03 -0500

    Update and rename ReplaceHyphens.py to BeautifulSoupReplaceHyphens.py

--- BeautifulSoupReplaceHyphens.py
+++ BeautifulSoupReplaceHyphens.py
@@ -3,13 +3,10 @@ from bs4 import BeautifulSoup
 
 
 soup=open("sarcastic-fringehead/sampleData/one.nxml", "lxml")
-#readText=justText.read()
-
-#tags=re.compile("<.+?-.+?>", re.DEBUG)
 
 for i in soup.findAll():
 	i.name=i.name.replace("-","_")
-#for article_id
+
 n=soup.find_all("article_id", attrs={"pub_id_type": "pmid"})
 m=n[0].contents
 
