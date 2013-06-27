e722a2da6f8e8d780ad46614d95d23d141afb6a3
akthom <andrea.thomer@gmail.com>
2013-06-27 16:56:45 -0500

    cleanup

--- ParatextExtractorVer1.py
+++ ParatextExtractorVer1.py
@@ -2,7 +2,6 @@
 
 #python 2.7.2 or NLTK won't work
 
-#path=("../sampleData")
 
 def ack1():
 
@@ -13,17 +12,10 @@ def ack1():
         import csv
 
 
-        outfile = open("preprocessedText3.txt","a")#, encoding="latin-1") <-- commented out because code was originally written for python 3.2 but then discovered that it broke 2.7
-        #also, outfile = original, untokenized text.  not really necessary but leaving in the code to show preprocessing step
-#        walk=os.walk(path)
-#        datalist=[]
-#        tally={}
+        outfile = open("preprocessedTextASIST.csv","a")#, encoding="latin-1") <-- commented out because code was originally written for python 3.2 but then discovered that it broke 2.7
 
-#        tokenizer=nltk.data.load('tokenizers/punkt/english.pickle') #modded from http://mailman.uib.no/public/corpora/2007-October/005426.html (thanks, internet!)
-#        summary=[]
-#        data=[]
         w=csv.writer(outfile)
-        w.writerow(["filename ","PMID ", "AcknowledgementsText"])
+        w.writerow(["filename ","PMID ", "Abstract ", "AcknowledgementsText"])
 
 
         for dirname, dirnames, filenames in os.walk('./sampleData'): #also modded from something on stack overflow that I now can't find
@@ -46,14 +38,14 @@ def ack1():
                                         pmid=pmidline[0].contents
 
 
-#                                pmid=pmidline[0].contents
                                 ack=soup.ack
                                 abstract=soup.abstract
                                 
-                                print(infile, str(pmid[0]), ack)
+                                print(infile, str(pmid[0]))
                                 w.writerow([filename, pmid[0], abstract, ack])
 
-                       
+        outfile.close()
+
 #                        if a.endswith('.txt'):
 #                                abstractNum=abstractNum+1
 #                                toWrite=str(a).strip(".txt")#+"|"+abstractNum+"|"
@@ -96,7 +88,7 @@ def ack1():
 #                        print(summary)
 
 #                w.writerow([filename, pmid[0], ack])
-        outfile.close()
+
 #        for key, val in summary.items():
 #                w.writerow([str(key)+"|"+str(val)])
 #              datalist=[]
